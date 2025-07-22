8ab49abc9290866157aa44e9a4fba15f0a329eeb
aj0urdain <agirton@intradark.com>
2025-07-23 01:15:33 +1000

    chore(intradark): update config and environment example

--- apps/intradark/next.config.mjs
+++ apps/intradark/next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "allstar.gg",
+      },
+      {
+        protocol: "https",
+        hostname: "mediacdn.allstar.gg",
+      },
+      {
+        protocol: "https",
+        hostname: "faceit-a.akamaihd.net",
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
