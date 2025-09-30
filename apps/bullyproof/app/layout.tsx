911ef8ece07de689384984fed0d2fdc77ae136aa
aj0urdain <agirton@intradark.com>
2025-09-30 21:15:58 +1000

    fix(bullyproof/layout): update app layout

--- apps/bullyproof/app/layout.tsx
+++ apps/bullyproof/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "@workspace/ui/globals.css";
-import { Providers } from "@/components/providers";
+import { Providers } from "@/providers/providers";
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
