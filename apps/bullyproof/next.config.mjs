55502e06b64d95d5c9f0400fc506cef434f7fa75
aj0urdain <agirton@intradark.com>
2025-10-10 19:33:41 +1100

    chore(bullyproof): update next config and package manifest

--- apps/bullyproof/next.config.mjs
+++ apps/bullyproof/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
