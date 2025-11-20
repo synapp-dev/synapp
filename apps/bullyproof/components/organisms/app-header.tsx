9ae5e0492067ec9ad96cd6c5789d524a285bb8c2
Aaron J. Girton <agirton@intradark.com>
2025-11-20 17:11:49 +1100

    refactor(bullyproof): update app header component

--- apps/bullyproof/components/organisms/app-header.tsx
+++ apps/bullyproof/components/organisms/app-header.tsx
@@ -5,8 +5,12 @@ import { ThemeToggle } from "@workspace/ui/components/atoms/theme-toggle";
 import { CommandMenu } from "@/components/molecules/command-menu";
 import { Breadcrumb } from "@/components/molecules/breadcrumb";
 import { ImpersonateMenu } from "@/components/molecules/impersonate-menu";
+import { useIsPlatformAdmin } from "@/entities/me/model/store";
 
 export function AppHeader() {
+
+  const isPlatformAdmin = useIsPlatformAdmin();
+
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 justify-between transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-14 sticky top-0 z-50 bg-background">
       <div className="flex items-center gap-2 px-4">
@@ -18,8 +22,12 @@ export function AppHeader() {
         <Breadcrumb />
       </div>
       <div className="flex items-center gap-2 px-4">
-        <ImpersonateMenu />
-        <div className="w-0.5 h-0.5 bg-muted-foreground rounded-full mx-2" />
+        {
+          isPlatformAdmin && (<>
+            <ImpersonateMenu />
+            <div className="w-0.5 h-0.5 bg-muted-foreground rounded-full mx-2" />
+          </>)
+        }
         <CommandMenu />
         <div className="w-0.5 h-0.5 bg-muted-foreground rounded-full mx-2" />
         <ThemeToggle />
