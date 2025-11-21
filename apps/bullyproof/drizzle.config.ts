8ebf456d76f222215062cd3256e563777e9d1fd2
Aaron J. Girton <agirton@intradark.com>
2025-11-21 13:06:55 +1100

    chore(bullyproof): update drizzle config

--- apps/bullyproof/drizzle.config.ts
+++ apps/bullyproof/drizzle.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
   schema: "./server/db/schema.ts",
+  schemaFilter: ["public"],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
