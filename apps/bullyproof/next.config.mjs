b3cfd3180f6085177fbdc3f336e6de58cf5f3628
Aaron J. Girton <agirton@intradark.com>
2025-12-03 17:16:14 +1100

    chore(bullyproof): update Next.js configuration

--- apps/bullyproof/next.config.mjs
+++ apps/bullyproof/next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "i.imgur.com",
       },
+      {
+        protocol: "https",
+        hostname: "sukurbtgprvxgoeagich.supabase.co",
+      },
     ],
   },
 };
