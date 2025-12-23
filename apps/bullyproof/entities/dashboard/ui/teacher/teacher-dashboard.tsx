fba8d64c96bafa7bda5deee33342299a609e55de
Aaron J. Girton <agirton@intradark.com>
2025-12-23 17:00:52 +1100

    refactor(bullyproof): update teacher dashboard

--- apps/bullyproof/entities/dashboard/ui/teacher/teacher-dashboard.tsx
+++ apps/bullyproof/entities/dashboard/ui/teacher/teacher-dashboard.tsx
@@ -1,3 +1,18 @@
+import { TeacherOverviewSection } from "@/entities/dashboard/ui/teacher/sections/overview/overview-section";
+import { TeacherHeroSection } from "@/entities/dashboard/ui/teacher/sections/hero/hero-section";
+import { Separator } from "@workspace/ui/components/separator";
+
 export function TeacherDashboard() {
-  return <div>TeacherDashboard</div>;
+  return (
+    <div className="space-y-6">
+      <TeacherHeroSection />
+
+      {/* Tab Menu */}
+      <div className="px-96 w-full">
+        <Separator className="my-10" />
+      </div>
+
+      <TeacherOverviewSection />
+    </div>
+  );
 }
