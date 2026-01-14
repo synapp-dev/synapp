b2eb696df60a92e771c4642c10d5714f6df228ee
Aaron J. Girton <agirton@intradark.com>
2026-01-15 05:15:38 +1100

    refactor(bullyproof): update school layout

--- apps/bullyproof/app/(main)/schools/[school_id]/layout.tsx
+++ apps/bullyproof/app/(main)/schools/[school_id]/layout.tsx
@@ -1,4 +1,5 @@
 import { SchoolStoreProviderWrapper } from "@/components/atoms/school-store-provider-wrapper";
+import { SchoolPageTutorialGuard } from "@/components/molecules/school-page-tutorial-guard";
 
 export default async function SchoolLayout({
   children,
@@ -14,6 +15,8 @@ export default async function SchoolLayout({
     <>
       {/* School Store Provider - populates the store with school data */}
       <SchoolStoreProviderWrapper slug={slug} />
+      {/* Tutorial Guard - shows tutorials on first visit to school pages */}
+      <SchoolPageTutorialGuard />
       {children}
     </>
   );
