cefca9a8fddc1fac87c58d96cd9b369c1ad1ac4b
Aaron J. Girton <agirton@intradark.com>
2026-02-10 21:57:21 +1100

    feat(bullyproof): add staff dashboard UI

---
+++ apps/bullyproof/entities/dashboard/ui/staff/staff-dashboard.tsx
@@ -0,0 +1,20 @@
+import { TeacherOverviewSection } from "@/entities/dashboard/ui/teacher/sections/overview/overview-section";
+import { TeacherHeroSection } from "@/entities/dashboard/ui/teacher/sections/hero/hero-section";
+import { Separator } from "@workspace/ui/components/separator";
+
+export function StaffDashboard() {
+  return (
+    <div className="space-y-6">
+      <TeacherHeroSection />
+
+      {/* Tab Menu */}
+      <div className="px-96 w-full">
+        <Separator className="my-10" />
+      </div>
+
+      <div>
+        <TeacherOverviewSection />
+      </div>
+    </div>
+  );
+}
